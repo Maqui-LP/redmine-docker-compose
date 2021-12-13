d8cc710c5b125dfe66ff669ffb94f8aa5e50fc28
Maqui-LP <maqui-lp@outlook.com>
2021-12-13 16:28:49 -0300

    fixed paths issues

--- redmine/puma.rb
+++ redmine/puma.rb
@@ -1,8 +1,9 @@
 #!/usr/bin/env puma
 
 application_path = '/usr/src/redmine'
-directory '/usr/src/redmine'
+directory application_path
 environment 'production'
-pidfile '/usr/src/redmine/tmp/pids/puma.pid'
-state_path '/usr/src/redmine/tmp/pids/puma.state'
-bind 'unix:///usr/src/redmine/tmp/sockets/puma.sock'
+pidfile "#{application_path}/pids/puma.pid"
+state_path "#{application_path}/pids/puma.state"
+bind "tcp:0.0.0.0:3000"
+# bind 'unix:///usr/src/redmine/sockets/puma.sock'
