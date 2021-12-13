cc68dfc795564f432b66e76cb9deb96548eef7d3
Maqui-LP <maqui-lp@outlook.com>
2021-12-13 17:40:24 -0300

    fixes

--- redmine/puma.rb
+++ redmine/puma.rb
@@ -5,5 +5,4 @@ directory application_path
 environment 'production'
 pidfile "#{application_path}/pids/puma.pid"
 state_path "#{application_path}/pids/puma.state"
-bind "tcp:0.0.0.0:3000"
-# bind 'unix:///usr/src/redmine/sockets/puma.sock'
+bind "tcp://0.0.0.0:3000"
