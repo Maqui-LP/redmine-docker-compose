1ac7450ddb4ed01745715c9a9484e450eb610da2
Maqui-LP <maqui-lp@outlook.com>
2021-12-13 14:23:07 -0300

    ruby image instead of redmine

--- redmine/entrypoint.sh
+++ redmine/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 bundle exec rake generate_secret_token
-bundle exec rake db:migrate RAILS_ENV="${ENV}"
+bundle exec rake db:migrate RAILS_ENV="${RAILS_ENV}"
 bundle exec rake redmine:load_default_data RAILS_ENV=production REDMINE_LANG=en
-puma -C config/puma.rb
+bundle exec puma -C config/puma.rb
