48e14f4733501d03876fd8739e12ac26f2f7a2bb
Maqui-LP <maqui-lp@outlook.com>
2021-12-13 09:04:57 -0300

    add entrypoint

---
+++ redmine/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+bundle exec rake generate_secret_token
+bundle exec rake db:migrate RAILS_ENV="${ENV}"
+bundle exec rake redmine:load_default_data RAILS_ENV=production REDMINE_LANG=en
+bundle exec puma -C config/puma.rb
