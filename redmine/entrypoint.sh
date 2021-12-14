6d55dae39f3f5954b2a1b0e093e33f06365e4d93
Maqui-LP <maqui-lp@outlook.com>
2021-12-14 17:30:51 -0300

    correcciones

--- redmine/entrypoint.sh
+++ redmine/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 bundle exec rake generate_secret_token
-bundle exec rake db:migrate RAILS_ENV="${RAILS_ENV}"
-bundle exec rake redmine:load_default_data RAILS_ENV="${RAILS_ENV}" REDMINE_LANG=en
-bundle exec puma -C config/puma.rb
+exec "$@"
+
+# bundle exec rake redmine:load_default_data RAILS_ENV="${RAILS_ENV}" REDMINE_LANG=en
