29df6407d6208b686f5716468d47e5df4945c817
Maqui-LP <maqui-lp@outlook.com>
2021-12-15 09:36:26 -0300

    network mode, fixed entrypoint, moved migrations

--- redmine/entrypoint.sh
+++ redmine/entrypoint.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env sh
 
 bundle exec rake generate_secret_token
-bundle exec rake db:migrate RAILS_ENV=production
-bundle exec puma -C config/puma.rb
+exec "$@"
 
-#exec "$@"
-
-# bundle exec rake redmine:load_default_data RAILS_ENV="${RAILS_ENV}" REDMINE_LANG=en
