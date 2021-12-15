0079eb59791651e3b3be99172322b8915871a4f2
Maqui-LP <maqui-lp@outlook.com>
2021-12-15 08:40:08 -0300

    volver atras

--- redmine/entrypoint.sh
+++ redmine/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 
 bundle exec rake generate_secret_token
-exec "$@"
+bundle exec rake db:migrate RAILS_ENV=production
+bundle exec puma -C config/puma.rb
+
+#exec "$@"
 
 # bundle exec rake redmine:load_default_data RAILS_ENV="${RAILS_ENV}" REDMINE_LANG=en
