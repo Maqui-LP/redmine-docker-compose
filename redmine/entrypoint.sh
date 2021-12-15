2507cc375bcffc5c9bea9b951be93985222b80c2
Maqui-LP <maqui-lp@outlook.com>
2021-12-15 13:49:58 -0300

    added secret_token and secret.yml

--- redmine/entrypoint.sh
+++ redmine/entrypoint.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env sh
 
-bundle exec rake generate_secret_token
-exec "$@"
+# if [ ! -f config/initializers/secret_token.rb ]; then
+#     bundle exec rake generate_secret_token
+# fi
 
+
+if [ ! -s config/secrets.yml ]; then
+	if [ -n "$REDMINE_SECRET_KEY_BASE" ]; then
+		cat > 'config/secrets.yml' <<-YML
+			production:
+				secret_key_base: "$REDMINE_SECRET_KEY_BASE"
+		YML
+	elif [ ! -f config/initializers/secret_token.rb ]; then
+		bundle exec rake generate_secret_token
+		REDMINE_SECRET_KEY_BASE=$(tail -c 83 config/initializers/secret_token.rb | tr -d "'")
+		export REDMINE_SECRET_KEY_BASE
+	fi
+fi
+
+exec "$@"
