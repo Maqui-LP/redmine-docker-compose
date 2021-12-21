ac268d178e95e0460f1a43a5580ef3f906486b63
Maqui-LP <maqui-lp@outlook.com>
2021-12-21 11:09:15 -0300

    correcciones post muestra

--- backup/entrypoint.sh
+++ backup/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+set -e
 printf "%s /usr/bin/rsnapshot hourly\n%s /usr/bin/rsnapshot daily\n%s /usr/bin/rsnapshot weekly" "${HOURLY_SCHEDULE}" "${DAILY_SCHEDULE}" "${WEEKLY_SCHEDULE}" | crontab -
 
 exec crond -f
